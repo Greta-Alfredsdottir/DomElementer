5f0a59467175a8afb1a1d8f726327ece5e58a258
Greta <asth0010@elev.techcollege.dk>
2025-03-17 13:24:49 +0100

    domelementer

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -1,13 +1,16 @@
 /* DOMcelementer */
 
 
-
 /* Opgave 1.1
 Du skal finde elementet med id="redFigure" og console logge det.
 Du skal finde elementet med id="yellowFigure" og console logge det. 
 */
 
 // din kode her
+const redFigure = document.getElementById('redFigure');
+console.log(redFigure);
+const yellowFigure = document.getElementById('yellowFigure');
+console.log(yellowFigure);
 
 
 /* opgave 1.2
@@ -17,8 +20,10 @@ farven på elementerne skal være blå. brug evt. elelement.style.backgroundColo
 
 
 // din kode her
-
-
+redFigure.style.backgroundColor = "blue";
+console.log(redFigure);
+yellowFigure.style.backgroundColor='blue';
+console.log(yellowFigure);
 
 /* opgave 2.1
 Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
@@ -28,7 +33,15 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 
 
 // din kode her
-
+let myElement = document.getElementById('opgaveTwo');
+let h2 = document.createElement('h2');
+h2.innerHTML = 'Opgave 2.1 løsning';
+let p = document.createElement('p');
+p.innerHTML = 'Jeg har løst opgave 2.1';
+myElement.appendChild(h2);
+myElement.appendChild(p);
+console.log(h2);
+console.log(p);
 
 // opgave 3 liveHTML lists
 /* opgave 3.1
@@ -36,6 +49,8 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 */
 
 // din kode her
+const purpleFiguresbla = document.querySelectorAll('.purpleFigures');
+console.log(purpleFiguresbla);
 
 
 /* opgave 3.2
@@ -46,6 +61,9 @@ eller bruge qurySelectorAll og foreach til at loope.
 */
 
 // din kode her
+let purpleFigures = Array.from(document.querySelectorAll('.purpleFigures'));
+
+purpleFigures.map(figure => figure.style.backgroundColor = 'red');
 
 
 /* opgave 3.3
@@ -55,6 +73,14 @@ myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 
 
 // din kode her
+purpleFigures = Array.from(document.querySelectorAll('.purpleFigures'));
+
+purpleFigures.map(figure => {
+    figure.style.backgroundColor = 'red'; // Ændrer baggrundsfarven til rød
+    if (figure.children.length > 0) {
+        figure.children[0].innerHTML = 'RED'; // Ændrer teksten i første child-element (forventet h3)
+    }
+});
 
 
 
@@ -72,6 +98,32 @@ const myData = {
 
 
 // din kode her
+// Find elementet med id "opgaveFour"
+const opgaveFour = document.getElementById('opgaveFour');
+
+// Opret et article-element for bedre semantik
+const article = document.createElement('article');
+
+// Opret et h2-element til navnet
+const title = document.createElement('h2');
+title.innerHTML = myData.name;
+
+// Opret et img-element til billedet
+const image = document.createElement('img');
+image.src = myData.image;
+image.alt = `Billede af en ${myData.name}`;
+
+// Opret et p-element til beskrivelsen
+const description = document.createElement('p');
+description.innerHTML = myData.description;
+
+// Tilføj elementerne til article
+article.appendChild(title);
+article.appendChild(image);
+article.appendChild(description);
+
+// Tilføj article til "opgaveFour"
+opgaveFour.appendChild(article);
 
 
 
